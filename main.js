e77c1dc3a2b9e3399a2e565304f9cfc54c403115
Hasan Berkay Çağır <berkay@cagir.com.tr>
2021-09-02 12:36:12 +0200

    Fixed a silly mistake

--- main.js
+++ main.js
@@ -38,67 +38,97 @@ class Type {
         this.prim = prim;
         this.annots = annots;
         this.args = args;
-        switch (this.prim.toString()) {
+        switch (this.prim) {
             case 'address':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'big_map':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.D];
+                break;
             case 'bls12_381_fr':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'bls12_381_g1':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'bls12_381_g2':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'bool':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'bytes':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'chain_id':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'contract':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.PA, TypeAttribute.D];
+                break;
             case 'int':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'key':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'key_hash':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'lambda':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'list':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'map':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'mutez':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'nat':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'never':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'operation':
                 this.attributes = [TypeAttribute.D];
+                break;
             case 'option':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'or':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'pair':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'sapling_state':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.D];
+                break;
             case 'sapling_transaction':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'set':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'signature':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'string':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'ticket':
                 this.attributes = [TypeAttribute.PM, TypeAttribute.S, TypeAttribute.B];
+                break;
             case 'timestamp':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             case 'unit':
                 this.attributes = [TypeAttribute.C, TypeAttribute.PM, TypeAttribute.S, TypeAttribute.PU, TypeAttribute.PA, TypeAttribute.B, TypeAttribute.D];
+                break;
             default:
                 throw('unknown data type '.concat(this.prim));
         }
@@ -106,7 +136,7 @@ class Type {
 }
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-const data = fs.readFileSync('/home/berkay/Nextcloud/Proje/Michelson/old/auction.tz', 'utf8');
+const data = fs.readFileSync('/Users/berkay/Nextcloud/Proje/Michelson/old/auction.tz', 'utf8');
 
 parser.feed(data);
 var result = JSON.parse(parser.results[0])
@@ -117,5 +147,4 @@ var stack = [];
 // examine parameter
 var parameter = result.shift().args.shift();
 stack.push(new Type(parameter.prim, parameter.args));
-console.log(parameter);
-console.log(result);
+console.log(stack);
