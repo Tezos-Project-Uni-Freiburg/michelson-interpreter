7847ab0560b793bc5b35a748287cff5d642e9b89
Ihr Name <Ihre E-Mail-Adresse>
2023-05-14 13:04:06 +0200

    index.cjs - export

---
+++ index.cjs
@@ -0,0 +1,4 @@
+import michelsonInterpreter from "michelson-interpreter/lib/michelson-interpreter-function.cjs";
+import { State } from "michelson-interpreter/lib/types.cjs";
+
+module.exports = {michelsonInterpreter, State};
