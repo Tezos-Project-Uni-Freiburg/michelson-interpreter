8bf4c1dec7f5609fc630089dd28162cb172b99e8
JanLazz <jan.lazzari@posteo.de>
2023-07-25 17:33:41 +0200

    export without default

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import michelsonInterpreter from "michelson-interpreter/lib/michelson-interpreter-function.cjs";
 import { State } from "michelson-interpreter/lib/types.cjs";
 
-export default { michelsonInterpreter, State };
+export { michelsonInterpreter, State };
 //export default michelsonInterpreter;
 //test cache
